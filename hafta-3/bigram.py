ec1c57cfcc891509c685167bb0eb90ccb61d52e7
Sena Eliri <senaeliri@gmail.com>
2026-09-01 20:00:26 +0300

    Hafta-3 görev 1 tamamlandı.

---
+++ hafta-3/bigram.py
@@ -0,0 +1,33 @@
+import torch
+import matplotlib.pyplot as plt
+
+words = open('names.txt', 'r').read().splitlines()
+
+
+chars = sorted(set(''.join(words)))
+stoi = {s:i +1 for i, s in enumerate(chars)}
+stoi["."] = 0
+
+
+d = torch.zeros((27, 27), dtype=torch.int32)
+
+for word in words:
+    word = ['.'] + list(word) + ['.']
+    for ch1, ch2 in zip(word, word[1:]):
+        i_of_ch1 = stoi[ch1]
+        i_of_ch2 = stoi[ch2]
+        d[i_of_ch1, i_of_ch2] += 1
+    
+itos = {i:s for s, i in stoi.items()}
+
+#visiuals
+plt.figure(figsize=(16, 16))
+plt.imshow(d, cmap='Blues')
+for i in range(27):
+    for j in range(27):
+        ch1xch2 = itos[i] + itos[j]
+        plt.text(j, i, ch1xch2, ha="center", va = "bottom", color= 'gray')
+        plt.text(j, i, d[i, j].item(), ha="center", va = "top",color='black' )
+
+plt.axis('off')
+plt.show()
