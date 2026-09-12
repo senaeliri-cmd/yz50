fb8093c3127b6e1b16d67e9b23cdb1a5805fed90
Sena Eliri <senaeliri@gmail.com>
2026-09-12 20:04:27 +0300

    Gizli katman ve embedding boyutu büyütüldü. Modelden isimler örneklendi.

--- hafta-4/forward_pass.py
+++ hafta-4/forward_pass.py
@@ -10,7 +10,9 @@ chars = sorted(set(''.join(words)))
 chars.insert(0, '.')
 
 stoi = {s : i for i, s in enumerate(chars)}
+itos = {i : s for i, s in enumerate(chars)}
 
+emb_size = 4
 base_num = 3
 X, Y = [], []
 
@@ -41,21 +43,22 @@ F = torch.nn.functional
 XS = torch.tensor(X)
 YS = torch.tensor(Y)
 
-C = torch.randn(len(chars), 2)
+C = torch.randn(len(chars), emb_size)
 
 emb = C[Xtr]
 
 g = torch.Generator().manual_seed(2147483647)
 
-#print(embd.shape) 32, 3, 2
-W1 = torch.randn((6,100), generator=g)
-B1 = torch.randn((100), generator=g)
+#print(embd.shape) 32, 3, emb_size(4)
+b = emb_size * base_num
+W1 = torch.randn((b,200), generator=g)
+B1 = torch.randn((200), generator=g)
 
-a = emb.view(-1, 6)
+a = emb.view(-1, emb_size * base_num)
 
 h = torch.tanh(a @ W1 + B1)
 
-W2= torch.randn((100, len(chars)), generator=g)
+W2= torch.randn((200, len(chars)), generator=g)
 B2 = torch.randn((len(chars)), generator=g)
 
 parameteres = [C, W1, B1, W2, B2]
@@ -85,7 +88,7 @@ for i in range(10000):
    #lr = lrs[i]
    Xi = torch.randint(0, Xtr.shape[0], (32,))
    emb = C[Xtr[Xi]] # (32, 3, 2)
-   h = torch.tanh(emb.view(-1, 6) @ W1 + B1) #(32, 100)
+   h = torch.tanh(emb.view(-1, b) @ W1 + B1) #(32, 200)
    logits = h @ W2 + B2 #(32, 27)
 
    loss = F.cross_entropy(logits, Ytr[Xi])
@@ -96,18 +99,18 @@ for i in range(10000):
 
    #lri.append(lre[i])
    #losses.append(loss.item())
-   lr = 0.01
+   lr = 0.1 if i < 5000 else 0.01
    for p in parameteres:
       p.data += - lr * p.grad
 
-embd = C[Xtr]
-h = torch.tanh(embd.view(-1, 6) @ W1 + B1)
+emb = C[Xtr]
+h = torch.tanh(emb.view(-1, b) @ W1 + B1)
 logits = h @W2 +B2
 loss = F.cross_entropy(logits, Ytr)
 print(f"Train Loss---->{loss.item()}")
  
-embd = C[Xdev]
-h = torch.tanh(embd.view(-1, 6) @ W1 + B1)
+emb = C[Xdev]
+h = torch.tanh(emb.view(-1, b) @ W1 + B1)
 logits = h @ W2 + B2
 loss = F.cross_entropy(logits, Ydev)
 print(f"Develop Loss--->{loss.item()}")
@@ -116,11 +119,38 @@ print(f"Develop Loss--->{loss.item()}")
 #plt.show()
 
 
+for _ in range(10):
+   context = [0] * base_num
+   out = []
+   while True:
+      emb = C[context]
+      h = torch.tanh(emb.view(-1, b) @ W1 + B1)
+      logits = h @ W2 + B2
+      probs = F.softmax(logits, dim=1)
+      ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g).item()
+      if(ix == 0):break
+      out.append(itos[ix])
+      context = context[1:] + [ix]
+   print(''.join(out))
 
-
-
-
-
-
-
-
+print("emb:", emb.shape)
+print("h:", h.shape)
+print("logits:", logits.shape)
+print("probs:", probs.shape)
+###
+#Train Loss---->2.365797519683838
+#Develop Loss--->2.3608527183532715
+#akka
+#tarnita
+#mellea
+#kawginalely
+#avyni
+#kanir
+#rak
+#gabteeniac
+#ean
+#niskan
+##
+#
+#
+#
