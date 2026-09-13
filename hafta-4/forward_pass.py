467a30f56ba93c0f80f10f6b9307e3d5bdcc04ad
Sena Eliri <senaeliri@gmail.com>
2026-09-13 15:54:02 +0300

    BatchNorm katmanı eklendi.

--- hafta-4/forward_pass.py
+++ hafta-4/forward_pass.py
@@ -52,14 +52,14 @@ g = torch.Generator().manual_seed(2147483647)
 
 #print(embd.shape) 32, 3, emb_size(4)
 b = emb_size * base_num
-
+n_hidden = 200
 
 a = emb.view(-1, emb_size * base_num)
 
 
-W1 = torch.randn((b,200), generator=g) * (5/3) / (b**0.5)
-B1 = torch.randn((200), generator=g) * 0.1
-W2= torch.randn((200, len(chars)), generator=g) * 0.01
+W1 = torch.randn((b,n_hidden), generator=g) * (5/3) / (b**0.5)
+B1 = torch.randn((n_hidden), generator=g) * 0.1
+W2= torch.randn((n_hidden, len(chars)), generator=g) * 0.01
 B2 = torch.randn((len(chars)), generator=g) * 0
 parameteres = [C, W1, B1, W2, B2]
 
@@ -73,9 +73,10 @@ for p in parameteres:
 #loss = -prob[torch.arange(32), YS].log().mean()
 
 #New Method
+batch_size = 32
 h = torch.tanh(a @ W1 + B1)
 logits = h @ W2 + B2
-Xi = torch.randint(0, Xtr.shape[0], (32,))
+Xi = torch.randint(0, Xtr.shape[0], (batch_size,))
 
 
 lre = torch.linspace(-3, 0, 1000)
@@ -83,12 +84,25 @@ lrs = 10**lre
 lri =[]
 losses = []
 
+bngain = torch.ones(1, n_hidden)
+bnbias = torch.zeros(1, n_hidden)
+bn_running_mean = torch.zeros(1, n_hidden)
+bn_running_std = torch.ones(1, n_hidden)
+
 for i in range(200000):
    #lr = lrs[i]
-   Xi = torch.randint(0, Xtr.shape[0], (32,))
+   Xi = torch.randint(0, Xtr.shape[0], (batch_size,))
    emb = C[Xtr[Xi]] # (32, 3, 2)
    hpreact = emb.view(-1, b) @ W1 + B1
-   h = torch.tanh(emb.view(-1, b) @ W1 + B1) #(32, 200)
+   bnmean = hpreact.mean(0, keepdim=True)
+   bnstd = hpreact.std(0, keepdim=True)
+
+   with torch.no_grad():
+      bn_running_mean = 0.999 *bn_running_mean + 0.001* bnmean
+      bn_running_std = 0.999 * bn_running_std + 0.001 * bnstd
+
+   hpreact = (bngain * (hpreact - bnmean) / (bnstd)) + (bnbias)
+   h = torch.tanh(hpreact) #(32, 200)
    logits = h @ W2 + B2 #(32, 27)
 
    loss = F.cross_entropy(logits, Ytr[Xi])
@@ -102,32 +116,34 @@ for i in range(200000):
    lr = 0.1 if i < 100000 else 0.01
    for p in parameteres:
       p.data += - lr * p.grad
-   break
+
    
 
-plt.figure(figsize=(20,10))
-plt.imshow(h.abs() > 0.99, cmap='gray', interpolation='nearest')
+#plt.figure(figsize=(20,10))
+#plt.imshow(h.abs() > 0.99, cmap='gray', interpolation='nearest')
 
 #plt.hist(h.view(-1).tolist(), 50)
 #plt.plot(lri, losses)
-plt.show()
+#plt.show()
 
+def sample_name():
+   for _ in range(10):
+      context = [0] * base_num
+      out = []
+      while True:
+         emb = C[context]
+         hpreact = emb.view(-1, b)@ W1
+         hpreact= bngain * (hpreact-bn_running_mean) / bn_running_std +bnbias
+         h = torch.tanh(hpreact)
+         logits = h @ W2 + B2
+         probs = F.softmax(logits, dim=1)
+         ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g).item()
+         if(ix == 0):break
+         out.append(itos[ix])
+         context = context[1:] + [ix]
+      print(''.join(out))
 
-for _ in range(10):
-   context = [0] * base_num
-   out = []
-   while True:
-      emb = C[context]
-      h = torch.tanh(emb.view(-1, b) @ W1 + B1)
-      logits = h @ W2 + B2
-      probs = F.softmax(logits, dim=1)
-      ix = torch.multinomial(probs, num_samples=1, replacement=True, generator=g).item()
-      if(ix == 0):break
-      out.append(itos[ix])
-      context = context[1:] + [ix]
-   #print(''.join(out))
-
-
+@torch.no_grad()
 def split_loss(split):
    x, y = {
       'train': (Xtr, Ytr), 
@@ -135,30 +151,43 @@ def split_loss(split):
       'test' : (Xte, Yte)
    }[split]
    emb = C[x]
-   h = torch.tanh(emb.view(-1, b)@W1 + B1)
+   hpreact = emb.view(-1, b) @ W1
+   hpreact = bngain * (hpreact- bn_running_mean)/bn_running_std + bnbias
+   h = torch.tanh(hpreact)
+   
    logits = h @ W2 + B2
    loss = F.cross_entropy(logits, y)
    print(f"{split} ---> {loss.item()}")
 
+
+
 split_loss('train')
 split_loss('develop')
+split_loss('test')
 
-##
-# # for 10.000 iterations
-#Train Loss---->2.365797519683838
-#Develop Loss--->2.3608527183532715
 
-##  for 200.000 iterations
-#train ---> 2.112308979034424
-#develop ---> 2.156257152557373
+# Baseline - 10,000 iterations
+# Train Loss   -> 2.3658
+# Dev Loss     -> 2.3609
 
-#düzenlenmeden önce başlangıç loss 21.0983 sonra 3.8490
+# Baseline - 200,000 iterations
+# Train Loss   -> 2.1123
+# Dev Loss     -> 2.1563
 
-## düzenlenmiş loss için 
-#train ---> 2.0738046169281006
-#develop ---> 2.1384427547454834
+# Initial loss before output-layer initialization fix
+# Initial Loss -> 21.0983
 
-#tanh saturasyon önelenmesi sonrası
-#train ---> 2.038963556289673
-#develop ---> 2.105384349822998
+# Initial loss after output-layer initialization fix
+# Initial Loss -> 3.8490
 
+# After fixing output-layer initialization
+# Train Loss   -> 2.0738
+# Dev Loss     -> 2.1384
+
+# After reducing tanh saturation with scaled W1 initialization
+# Train Loss   -> 2.0390
+# Dev Loss     -> 2.1054
+
+# After batch-nom
+# Train Loss   -> 2.089315414428711
+# Dev Loss     -> 2.1237263679504395
