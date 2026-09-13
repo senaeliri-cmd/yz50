55ebc4a9e9a3f55dda90e3b47a92d33a88b721f4
Sena Eliri <senaeliri@gmail.com>
2026-09-13 14:16:36 +0300

    Saturasyon engellendi. Ağırlıklar ölçeklendi.

--- hafta-4/forward_pass.py
+++ hafta-4/forward_pass.py
@@ -2,6 +2,7 @@ import torch
 import matplotlib.pyplot as plt
 import build_dataset
 import random
+import torch.nn.functional as F
 
 
 words = open('names.txt', 'r').read().splitlines()
@@ -12,7 +13,7 @@ chars.insert(0, '.')
 stoi = {s : i for i, s in enumerate(chars)}
 itos = {i : s for i, s in enumerate(chars)}
 
-emb_size = 4
+emb_size = 10
 base_num = 3
 X, Y = [], []
 
@@ -38,7 +39,7 @@ Xtr, Ytr = build_dataset.build_dataset(words[:n1])
 Xdev, Ydev = build_dataset.build_dataset(words[n1:n2])
 Xte, Yte = build_dataset.build_dataset(words[n2:])
 
-F = torch.nn.functional
+
 
 XS = torch.tensor(X)
 YS = torch.tensor(Y)
@@ -51,18 +52,19 @@ g = torch.Generator().manual_seed(2147483647)
 
 #print(embd.shape) 32, 3, emb_size(4)
 b = emb_size * base_num
-W1 = torch.randn((b,200), generator=g)
-B1 = torch.randn((200), generator=g)
+
 
 a = emb.view(-1, emb_size * base_num)
 
-h = torch.tanh(a @ W1 + B1)
 
-W2= torch.randn((200, len(chars)), generator=g)
-B2 = torch.randn((len(chars)), generator=g)
-
+W1 = torch.randn((b,200), generator=g) * (5/3) / (b**0.5)
+B1 = torch.randn((200), generator=g) * 0.1
+W2= torch.randn((200, len(chars)), generator=g) * 0.01
+B2 = torch.randn((len(chars)), generator=g) * 0
 parameteres = [C, W1, B1, W2, B2]
 
+for p in parameteres:
+   p.requires_grad=True
 
 #Old method
 #logits = h @ W2 + B2
@@ -71,23 +73,21 @@ parameteres = [C, W1, B1, W2, B2]
 #loss = -prob[torch.arange(32), YS].log().mean()
 
 #New Method
+h = torch.tanh(a @ W1 + B1)
 logits = h @ W2 + B2
 Xi = torch.randint(0, Xtr.shape[0], (32,))
 
 
-
-
-for p in parameteres:
-   p.requires_grad=True
 lre = torch.linspace(-3, 0, 1000)
 lrs = 10**lre
 lri =[]
 losses = []
 
-for i in range(10000):
+for i in range(200000):
    #lr = lrs[i]
    Xi = torch.randint(0, Xtr.shape[0], (32,))
    emb = C[Xtr[Xi]] # (32, 3, 2)
+   hpreact = emb.view(-1, b) @ W1 + B1
    h = torch.tanh(emb.view(-1, b) @ W1 + B1) #(32, 200)
    logits = h @ W2 + B2 #(32, 27)
 
@@ -99,24 +99,18 @@ for i in range(10000):
 
    #lri.append(lre[i])
    #losses.append(loss.item())
-   lr = 0.1 if i < 5000 else 0.01
+   lr = 0.1 if i < 100000 else 0.01
    for p in parameteres:
       p.data += - lr * p.grad
+   break
+   
 
-emb = C[Xtr]
-h = torch.tanh(emb.view(-1, b) @ W1 + B1)
-logits = h @W2 +B2
-loss = F.cross_entropy(logits, Ytr)
-print(f"Train Loss---->{loss.item()}")
- 
-emb = C[Xdev]
-h = torch.tanh(emb.view(-1, b) @ W1 + B1)
-logits = h @ W2 + B2
-loss = F.cross_entropy(logits, Ydev)
-print(f"Develop Loss--->{loss.item()}")
+plt.figure(figsize=(20,10))
+plt.imshow(h.abs() > 0.99, cmap='gray', interpolation='nearest')
 
+#plt.hist(h.view(-1).tolist(), 50)
 #plt.plot(lri, losses)
-#plt.show()
+plt.show()
 
 
 for _ in range(10):
@@ -131,26 +125,40 @@ for _ in range(10):
       if(ix == 0):break
       out.append(itos[ix])
       context = context[1:] + [ix]
-   print(''.join(out))
+   #print(''.join(out))
 
-print("emb:", emb.shape)
-print("h:", h.shape)
-print("logits:", logits.shape)
-print("probs:", probs.shape)
-###
+
+def split_loss(split):
+   x, y = {
+      'train': (Xtr, Ytr), 
+      'develop' : (Xdev, Ydev),
+      'test' : (Xte, Yte)
+   }[split]
+   emb = C[x]
+   h = torch.tanh(emb.view(-1, b)@W1 + B1)
+   logits = h @ W2 + B2
+   loss = F.cross_entropy(logits, y)
+   print(f"{split} ---> {loss.item()}")
+
+split_loss('train')
+split_loss('develop')
+
+##
+# # for 10.000 iterations
 #Train Loss---->2.365797519683838
 #Develop Loss--->2.3608527183532715
-#akka
-#tarnita
-#mellea
-#kawginalely
-#avyni
-#kanir
-#rak
-#gabteeniac
-#ean
-#niskan
-##
-#
-#
-#
+
+##  for 200.000 iterations
+#train ---> 2.112308979034424
+#develop ---> 2.156257152557373
+
+#düzenlenmeden önce başlangıç loss 21.0983 sonra 3.8490
+
+## düzenlenmiş loss için 
+#train ---> 2.0738046169281006
+#develop ---> 2.1384427547454834
+
+#tanh saturasyon önelenmesi sonrası
+#train ---> 2.038963556289673
+#develop ---> 2.105384349822998
+
